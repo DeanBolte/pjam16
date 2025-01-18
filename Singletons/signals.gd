9973ba0107cd96369a8beec3be5dbfd23a4f9434
dean <dean.bolte7@gmail.com>
2025-01-18 16:43:11 +1100

    basic room tranisitions

---
+++ Singletons/signals.gd
@@ -0,0 +1,3 @@
+extends Node2D
+
+signal player_entered_room(new_position: Vector2)
--- room_camera/room_camera.gd
+++ room_camera/room_camera.gd
@@ -1,1 +1,7 @@
 extends Camera2D
+
+func _ready() -> void:
+	Signals.player_entered_room.connect(_move_camera_to_player)
+
+func _move_camera_to_player(new_position: Vector2):
+	global_position = new_position
