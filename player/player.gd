9c26052b92ed187975b8479774a2d070310bc289
Brodey Yendall <brodey_yendall@hotmail.com>
2025-01-18 22:07:31 +1100

    The sword goes "out" in higher speeds so it looks like the peasant is dragged.

--- player/player.gd
+++ player/player.gd
@@ -10,15 +10,21 @@ const ROTATION_SPEED = 0.5
 @export var base_speed: int = 20
 @export var speed_multiplier = 2
 
+var sword_starting_x = 0
+
+func _ready() -> void:
+	sword_starting_x = $sword.position.x
+
 func _physics_process(delta: float) -> void:
 	var mouse_position = get_global_mouse_position()
 	var mouse_direction = global_position.direction_to(mouse_position)
 	var distanceFromMouse = $sword_tip.global_position.distance_to(mouse_position)
 
-	if Input.is_action_pressed("stop_moving"):
-		velocity = Vector2(0, 0)
-	else:
-		velocity = mouse_direction * min(max_speed, (distanceFromMouse + base_speed) * speed_multiplier)
+	var speed = 0
+	if not Input.is_action_pressed("stop_moving"):
+		speed = min(max_speed, (distanceFromMouse + base_speed) * speed_multiplier)
+	velocity = mouse_direction * speed
+	$sword.position.x = sword_starting_x + (speed / 30)
 
 	if distanceFromMouse > 20 and global_position.distance_to(mouse_position) > 20:
 		rotation = lerp_angle(rotation, mouse_direction.angle(), rotation_speed)
