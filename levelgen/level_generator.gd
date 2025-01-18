95d7f184baf417d4c3d4708b3d472f956cb950f7
dean <dean.bolte7@gmail.com>
2025-01-18 13:49:32 +1100

    multiple room generation

--- levelgen/level_generator.gd
+++ levelgen/level_generator.gd
@@ -2,9 +2,49 @@ extends Node
 
 var ROOM_RESOURCE = preload("res://levelgen/room/room.tscn")
 
+@export var MAX_ROOMS := 30
+
+# a Map of 2D Vector keys representing the room grid
+var level_map: Dictionary = {}
+
 
 func _ready() -> void:
 	var base_room: Room = ROOM_RESOURCE.instantiate()
 	add_child(base_room)
 
-	base_room.generate()
+	base_room.generate(Vector2i.ZERO)
+	level_map[Vector2i.ZERO] = base_room
+
+	_generate_map()
+
+# ------- map generation
+func _generate_map() -> void:
+	while MAX_ROOMS - level_map.size() > 0:
+		var vacant_rooms := _get_room_candidates()
+		vacant_rooms.shuffle()
+
+		var room: Room = ROOM_RESOURCE.instantiate()
+		add_child(room)
+
+		var location = vacant_rooms.front()
+		room.generate(location)
+		level_map[location] = room
+
+func _get_room_candidates() -> Array[Vector2i]:
+	var vacant_rooms: Array[Vector2i] = []
+	for room: Vector2i in level_map.keys():
+		if _is_room_vacant(Vector2i(room.x - 1, room.y)):
+			vacant_rooms.append(Vector2i(room.x - 1, room.y))
+		if _is_room_vacant(Vector2i(room.x + 1, room.y)):
+			vacant_rooms.append(Vector2i(room.x + 1, room.y))
+		if _is_room_vacant(Vector2i(room.x, room.y + 1)):
+			vacant_rooms.append(Vector2i(room.x, room.y + 1))
+		if _is_room_vacant(Vector2i(room.x + 1, room.y)):
+			vacant_rooms.append(Vector2i(room.x + 1, room.y))
+	return vacant_rooms
+
+func _does_room_exist(room_location: Vector2i) -> bool:
+	return level_map.has(room_location)
+
+func _is_room_vacant(room_location: Vector2i) -> bool:
+	return not level_map.has(room_location)
--- levelgen/room/room.gd
+++ levelgen/room/room.gd
@@ -3,10 +3,24 @@ class_name Room
 
 var CRATE_RESOURCE := preload("res://levelgen/room/objects/crate.tscn")
 
+var ROOM_WIDTH := 640
+var ROOM_HEIGHT := 640
+
 @onready var Objects := $Objects
+@onready var LeftDoor := $LeftDoor
+@onready var RightDoor := $RightDoor
+@onready var BottomDoor := $BottomDoor
+@onready var TopDoor := $TopDoor
 
-@export var MAX_CRATES := 40
+@export var MAX_CRATES := 4
 
+@export var SPAWN_AREA_WIDTH: int
+@export var SPAWN_AREA_HEIGHT: int
+
+var _leftDoorConnected := true
+var _rightDoorConnected := true
+var _topDoorConnected := true
+var _bottomDoorConnected := true
 
 func _generate_objects() -> void:
 	_generate_crates()
@@ -20,11 +34,19 @@ func _generate_crates() -> void:
 
 
 func _get_spawn_location() -> Vector2:
-	return Vector2.ZERO
+	return Vector2(randi() % SPAWN_AREA_WIDTH - SPAWN_AREA_WIDTH / 2, randi() % SPAWN_AREA_HEIGHT - SPAWN_AREA_HEIGHT / 2)
 
 func _is_location_free() -> bool:
 	return true
 
 
-func generate() -> void:
+func generate(location: Vector2i) -> void:
+	global_position = Vector2(location.x * ROOM_WIDTH , location.y * ROOM_HEIGHT)
+
 	_generate_objects()
+
+func close_doors() -> void:
+	TopDoor.enabled = not _topDoorConnected
+	BottomDoor.enabled = not _bottomDoorConnected
+	RightDoor.enabled = not _rightDoorConnected
+	LeftDoor.enabled = not _leftDoorConnected
