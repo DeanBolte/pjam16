2692506d69eb9d2a0fb477f2239204ea085d122d
Brodey Yendall <brodey_yendall@hotmail.com>
2025-01-18 21:18:34 +1100

    Moved player to the current folder

--- player/player.gd
+++ player/player.gd
@@ -10,8 +10,6 @@ const ROTATION_SPEED = 0.5
 @export var base_speed: int = 20
 @export var speed_multiplier = 2
 
-var mouse_is_in_window = true
-
 func _physics_process(delta: float) -> void:
 	var mouse_position = get_global_mouse_position()
 	var mouse_direction = global_position.direction_to(mouse_position)
@@ -22,14 +20,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = mouse_direction * min(max_speed, (distanceFromMouse + base_speed) * speed_multiplier)
 	
-	if (distanceFromMouse > 20 and global_position.distance_to(mouse_position) > 20) and mouse_is_in_window:
+	if distanceFromMouse > 20 and global_position.distance_to(mouse_position) > 20:
 		rotation = lerp_angle(rotation, mouse_direction.angle(), rotation_speed) 
 		move_and_slide()
-	
-func _notification(event):
-	# TODO We probably wont to remove this later for an actual pause mechanic, I have this here for easier testing. 
-	match event:
-		NOTIFICATION_WM_MOUSE_EXIT:
-			mouse_is_in_window = false
-		NOTIFICATION_WM_MOUSE_ENTER:
-			mouse_is_in_window = true
