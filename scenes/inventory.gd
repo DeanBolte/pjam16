5bbbba5227e8fa04d60f7b7afd6cecf0c963958e
Brodey Yendall <brodey_yendall@hotmail.com>
2025-01-19 13:09:09 +1100

    Fix ups (Dylan was in call)

--- scenes/inventory.gd
+++ scenes/inventory.gd
@@ -1,11 +1,40 @@
-extends CanvasLayer
+extends Control
 
+@export var inventory_size := 10
+@export var selected_size := 3
 
-# Called when the node enters the scene tree for the first time.
+var items_load = [
+	"res://ui/development/sword.tres",
+	"res://ui/development/emerald.tres"
+]
+
+var selected_items_load = [
+	"res://ui/development/diamond.tres"
+]
+
 func _ready():
-	pass # Replace with function body.
+	self.visible = false
+	
+	for i in inventory_size:
+		var slot := InventorySlot.new()
+		slot.init(ItemData.Type.MAIN, Vector2(64, 64))
+		%Inv.add_child(slot)
+	
+	for i in selected_size:
+		var selected_slot := InventorySlot.new()
+		selected_slot.init(ItemData.Type.MAIN, Vector2(96, 96))	
+		%SelectedItems.add_child(selected_slot)
+	
+	for i in items_load.size():
+		var item := InventoryItem.new()
+		item.init(load(items_load[i]))
+		%Inv.get_child(i).add_child(item)
+	
+	for i in selected_items_load.size():
+		var selected_item := InventoryItem.new()
+		selected_item.init(load(selected_items_load[i]))
+		%SelectedItems.get_child(i).add_child(selected_item)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("inventory"):
+		self.visible = !self.visible
