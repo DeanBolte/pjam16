48153295ec21985b59aeae27b78ff72318e7f487
GitAYYYer <duc.h@hotmail.com>
2025-01-23 22:40:46 +1100

    runs like doodoo

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -4,36 +4,35 @@ extends CharacterBody2D
 @export var health: int = 3
 @export var invincibility_time: float = 2 # seconds of invincibility after being hit
 
-var random_direction: Vector2 = Vector2.ZERO
 var is_invincible: bool = false
 
+var player: CharacterBody2D = null
+
 func _ready():
+	# Not the best way to do this, but works for now
+	player = get_tree().get_nodes_in_group("Players")[0]
 	$invincibility_timer.wait_time = invincibility_time
 	$invincibility_timer.one_shot = true
-	set_random_direction()
 
-func _process(delta):
-	patrol(delta)
-
-func patrol(delta):
-	velocity = random_direction * speed * delta
+func _physics_process(delta: float) -> void:
+	calc_path()
+		
+func calc_path():
+	var target_position = player.global_position
+	$NavigationAgent2D.target_position = target_position
+	
+	var current_position = global_position
+	var next_path_position = $NavigationAgent2D.get_next_path_position()
+	var new_velocity = current_position.direction_to(next_path_position) * speed
+	
+	if ($NavigationAgent2D.is_navigation_finished()):
+		return
+	
+	if ($NavigationAgent2D.avoidance_enabled):
+		$NavigationAgent2D.set_velocity(new_velocity)
+	else:
+		_on_navigation_agent_2d_velocity_computed(new_velocity)
 	move_and_slide()
-
-	if should_change_direction():
-		set_random_direction()
-
-func set_random_direction():
-	random_direction = Vector2(
-		randi() % 3 - 1,  # -1, 0, or 1 for x-axis
-		randi() % 3 - 1   # -1, 0, or 1 for y-axis
-	).normalized()
-
-	if random_direction == Vector2.ZERO:
-		set_random_direction()
-
-# 1% chance that per frame, the enemy will decide to change direction
-func should_change_direction() -> bool:
-	return randi() % 100 < 1
 
 func on_hit():
 	if is_invincible:
@@ -66,3 +65,7 @@ func start_invincibility():
 func end_invincibility():
 	is_invincible = false
 	$sprite.modulate = Color(1, 1, 1, 1)
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
+	pass
